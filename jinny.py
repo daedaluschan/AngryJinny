11c740b97201c8beb93875d7978524179f018a7d
U425651 <daedalus.chan@wellsfargo.com>
2016-02-11 13:15:03 +0800

    delegator bot

--- jinny.py
+++ jinny.py
@@ -2,10 +2,10 @@ __author__ = 'daedaluschan'
 import sys
 import time
 import telepot
+from telepot.delegate import per_chat_id, create_open
 
 from datetime import date
 from types import *
-
 
 class Jinny:
   myName = "Jinny Chan"
@@ -28,6 +28,35 @@ class Jinny:
       theDate = whichDateIn
 
     return (theDate - Jinny.myDOB).days + 1
+
+class AngryJinny(telepot.helper.ChatHandler):
+    def __init__(self, seed_tuple, timeout):
+        super(AngryJinny, self).__init__(seed_tuple, timeout)
+        self._asking_date = False
+        print('constructor is being called')
+
+    def on_message(self, msg):
+        print('on_message() is being called')
+        flavor = telepot.flavor(msg)
+
+        # normal message
+        if flavor == 'normal':
+            content_type, chat_type, chat_id = telepot.glance2(msg)
+            print('Normal Message:', content_type, chat_type, chat_id, '; message content: ', msg)
+
+            if msg['text'] == '/start' or msg['text'] == '/today':
+                self.sendMessage(chat_id=chat_id, text='Today is ' + str(date.today()) + '. \n' +
+                                                       'It is Jinny\'s day ' + str(Jinny.getNumOfDays()) + '. \n' +
+                                                       'Use /help for more options')
+            elif msg['text'] == '/help':
+                self.sendMessage(chat_id=chat_id, text='/today - get today\'s date and Jinny\'s day. \n' +
+                                                       '/help - help menu. \n' +
+                                                       '/query - check the number of days for a particular date. \n')
+            else:
+                self.sendMessage(chat_id=chat_id, text='I don\'t understand what you are saying !\n' +
+                                                       'please try again or use /help for assistance.')
+        else:
+            raise telepot.BadFlavor(msg)
 
 class YourBot(telepot.Bot):
     def handle(self, msg):
@@ -74,10 +103,11 @@ class YourBot(telepot.Bot):
 
 TOKEN = sys.argv[1]  # get token from command-line
 
-bot = YourBot(TOKEN)
-bot.notifyOnMessage()
+bot = telepot.DelegatorBot(TOKEN, [
+    (per_chat_id(), create_open(AngryJinny, timeout=10)),])
+bot.notifyOnMessage(run_forever=True)
 print('Listening ...')
 
 # Keep the program running.
-while 1:
-    time.sleep(10)
+# while 1:
+#     time.sleep(10)
