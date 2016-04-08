3a65e592c553a30cc463a5d5d50fb9ac0617e481
Daedalus Chan <daedalus.chan@gmail.com>
2016-04-08 20:46:58 +0800

    write list to file

--- chk_n_conv.py
+++ chk_n_conv.py
@@ -1,7 +1,9 @@
 def chkNConv(instr):
     if isinstance(instr, str):
+        print('DEBUG: is str')
         return instr.encode(encoding='utf-8')
     elif isinstance(instr, unicode):
+        print('DEBUG: is unicode')
         return instr
     else:
         return u''
