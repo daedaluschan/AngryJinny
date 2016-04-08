5939b7d924f9e74d1c5e6b894bb71831de09faf4
Daedalus Chan <daedalus.chan@gmail.com>
2016-04-08 18:37:30 +0800

    to buy list

--- jinny.py
+++ jinny.py
@@ -11,11 +11,14 @@ from chk_n_conv import  chkNConv
 from enum import Enum
 
 to_buy_list = []
+white_list = [161517202]
+
 
 class ConverType(Enum):
     nothing = 1
     asking_date = 2
     adding_to_buy = 3
+
 
 class Jinny:
   myName = u'Jinny Chan'
@@ -38,6 +41,7 @@ class Jinny:
       theDate = whichDateIn
 
     return (theDate - Jinny.myDOB).days + 1
+
 
 class AngryJinny(telepot.helper.ChatHandler):
     def __init__(self, seed_tuple, timeout):
@@ -67,52 +71,57 @@ class AngryJinny(telepot.helper.ChatHandler):
             content_type, chat_type, _chat_id = telepot.glance2(msg)
             print('Normal Message:', content_type, chat_type, _chat_id, '; message content: ', msg)
 
-            if self._convert_type == ConverType.nothing:
-                if chkNConv(msg['text']) == u'/start' or chkNConv(msg['text']) == u'/today' or chkNConv(msg['text']) == u'今日 day 幾':
-                    self.sender.sendMessage(text=u'Today is ' + chkNConv(str(date.today())) + u'. \n' +
-                                                           u'It is my day ' + chkNConv(str(Jinny.getNumOfDays())) + u'. \n' +
-                                                           u'Use /help for more options',
-                                            reply_markup=self.genKeyboard())
-                elif chkNConv(msg['text']) == u'/help':
-                    self.sender.sendMessage(text=u'/today - get today\'s date and my day count. \n' +
-                                                           u'/help - for those who have bad memory. \n' +
-                                                           u'/query - check my day count for a particular date. \n',
-                                            reply_markup=self.genKeyboard())
-                elif chkNConv(msg['text']) == u'/query' or chkNConv(msg['text']) == u'某日係 day 幾':
-                    # self._asking_date = True
-                    self._convert_type = ConverType.asking_date
-                    self.sender.sendMessage(text=u'邊日呀 ?', reply_markup={'hide_keyboard': True})
-                elif chkNConv(msg['text']) == u'/No' :
-                    # self._asking_date = False
-                    self._convert_type = ConverType.nothing
-                    self.sender.sendMessage(text=u'Bye !',
-                                            reply_markup=self.genKeyboard())
-                elif chkNConv(msg['text']) == u'有野要買':
-                    self._convert_type = ConverType.adding_to_buy
-                    self.sender.sendMessage(text=u'買乜？', reply_markup={'hide_keyboard': True})
-                elif chkNConv(msg['text']) == u'有乜未買？':
-                    self.sender.sendMessage(text=self.genBuyList(), reply_markup=self.genKeyboard())
+            if _chat_id in white_list :
+
+                if self._convert_type == ConverType.nothing:
+                    if chkNConv(msg['text']) == u'/start' or chkNConv(msg['text']) == u'/today' or chkNConv(msg['text']) == u'今日 day 幾':
+                        self.sender.sendMessage(text=u'Today is ' + chkNConv(str(date.today())) + u'. \n' +
+                                                               u'It is my day ' + chkNConv(str(Jinny.getNumOfDays())) + u'. \n' +
+                                                               u'Use /help for more options',
+                                                reply_markup=self.genKeyboard())
+                    elif chkNConv(msg['text']) == u'/help':
+                        self.sender.sendMessage(text=u'/today - get today\'s date and my day count. \n' +
+                                                               u'/help - for those who have bad memory. \n' +
+                                                               u'/query - check my day count for a particular date. \n',
+                                                reply_markup=self.genKeyboard())
+                    elif chkNConv(msg['text']) == u'/query' or chkNConv(msg['text']) == u'某日係 day 幾':
+                        # self._asking_date = True
+                        self._convert_type = ConverType.asking_date
+                        self.sender.sendMessage(text=u'邊日呀 ?', reply_markup={'hide_keyboard': True})
+                    elif chkNConv(msg['text']) == u'/No' :
+                        # self._asking_date = False
+                        self._convert_type = ConverType.nothing
+                        self.sender.sendMessage(text=u'Bye !',
+                                                reply_markup=self.genKeyboard())
+                    elif chkNConv(msg['text']) == u'有野要買':
+                        self._convert_type = ConverType.adding_to_buy
+                        self.sender.sendMessage(text=u'買乜？', reply_markup={'hide_keyboard': True})
+                    elif chkNConv(msg['text']) == u'有乜未買？':
+                        self.sender.sendMessage(text=self.genBuyList(), reply_markup=self.genKeyboard())
+                    else:
+                        self.sender.sendMessage(text=u'我唔明呀。\n' +
+                                                     u'你試多次啦。或者用 /help 我就教你用。')
+                elif self._convert_type == ConverType.asking_date:
+                    try :
+                        self.sender.sendMessage(text=u'For ' + chkNConv(msg['text']) + u', It is my day ' +
+                                                     chkNConv(str(Jinny.getNumOfDaysSpecific(msg['text']))) +
+                                                     u'. Any other date to ask ? /No ?')
+                    except BaseException :
+                        self.sender.sendMessage(text=u'Not a data that I can understand ! \n' +
+                                                     u'What date (YYYYMMDD) ? \nOr you done ? ( /No )')
+                elif self._convert_type == ConverType.adding_to_buy:
+                    if chkNConv(msg['text']) == u'/done':
+                        self._convert_type = ConverType.nothing
+                        self.sender.sendMessage(text=u'Bye', reply_markup=self.genKeyboard())
+                    else:
+                        to_buy_list.append(chkNConv(msg['text']))
+                        self.sender.sendMessage(text=u'仲有冇？如果冇，就用 /done 完結。', reply_markup={'hide_keyboard': True})
                 else:
                     self.sender.sendMessage(text=u'我唔明呀。\n' +
                                                  u'你試多次啦。或者用 /help 我就教你用。')
-            elif self._convert_type == ConverType.asking_date:
-                try :
-                    self.sender.sendMessage(text=u'For ' + chkNConv(msg['text']) + u', It is my day ' +
-                                                 chkNConv(str(Jinny.getNumOfDaysSpecific(msg['text']))) +
-                                                 u'. Any other date to ask ? /No ?')
-                except BaseException :
-                    self.sender.sendMessage(text=u'Not a data that I can understand ! \n' +
-                                                 u'What date (YYYYMMDD) ? \nOr you done ? ( /No )')
-            elif self._convert_type == ConverType.adding_to_buy:
-                if chkNConv(msg['text']) == u'/done':
-                    self._convert_type = ConverType.nothing
-                    self.sender.sendMessage(text=u'Bye', reply_markup=self.genKeyboard())
-                else:
-                    to_buy_list.append(chkNConv(msg['text']))
-                    self.sender.sendMessage(text=u'仲有冇？如果冇，就用 /done 完結。', reply_markup={'hide_keyboard': True})
+
             else:
-                self.sender.sendMessage(text=u'我唔明呀。\n' +
-                                             u'你試多次啦。或者用 /help 我就教你用。')
+                self.sender.sendMessage(text=u'你邊位呀？')
         else:
             raise telepot.BadFlavor(msg)
 
