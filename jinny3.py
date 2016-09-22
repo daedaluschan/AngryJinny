544e9f13ca45ffef401b74944924381fc72d0bf4
Daedalus Chan <daedalus.chan@gmail.com>
2016-09-23 00:07:20 +0800

    handle bad flavor

--- jinny3.py
+++ jinny3.py
@@ -97,7 +97,7 @@ class AngryJinny(telepot.helper.ChatHandler):
 
         # normal message
         if flavor == 'chat':
-            content_type, chat_type, _chat_id = telepot.glance2(msg)
+            content_type, chat_type, _chat_id = telepot.glance(msg)
             print('Normal Message:', content_type, chat_type, _chat_id, '; message content: ', msg)
 
             if _chat_id in white_list :
