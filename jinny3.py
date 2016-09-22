ee5a737596091de61ad45153bb18a6817cdd16c3
Daedalus Chan <daedalus.chan@gmail.com>
2016-09-23 00:21:52 +0800

    no need convert unicode

--- jinny3.py
+++ jinny3.py
@@ -62,13 +62,13 @@ class AngryJinny(telepot.helper.ChatHandler):
         show_keyboard = {'keyboard': [[u'今日 day 幾',u'某日係 day 幾'], [u'有乜未買？', u'有野要買']]}
 
         for idx, item in enumerate(to_buy_list):
-            show_keyboard['keyboard'].append([u'買左[' + chkNConv(idx.__str__()) + u']： ' + chkNConv(item)])
+            show_keyboard['keyboard'].append([u'買左[' + idx.__str__() + u']： ' + item)]
         return show_keyboard
 
     def genBuyList(self):
         buy_list = u'要買呢D：\n\n'
         for item in to_buy_list:
-            buy_list = buy_list + u'- ' + chkNConv(item) + u'\n'
+            buy_list = buy_list + u'- ' + item + u'\n'
 
         return buy_list
 
@@ -90,7 +90,7 @@ class AngryJinny(telepot.helper.ChatHandler):
         self.writeListToFile()
         # self.sender.sendMessage(text=u'Okay。依家仲' + self.genBuyList(), reply_markup=self.genKeyboard())
         for white_user in white_list:
-            self.bot.sendMessage(white_user, text=u'買左：' + chkNConv(item_name), reply_markup=self.genKeyboard())
+            self.bot.sendMessage(white_user, text=u'買左：' + item_name, reply_markup=self.genKeyboard())
 
     def on_message(self, msg):
         print('on_message() is being called')
@@ -108,28 +108,28 @@ class AngryJinny(telepot.helper.ChatHandler):
                 if self._convert_type == ConverType.nothing:
                     if msg['text'] == u'/start' or msg['text'] == u'/today' or msg['text'] == u'今日 day 幾':
                         self.sender.sendMessage(text=u'Today is ' + str(date.today()) + u'. \n' +
-                                                     u'It is my day ' + chkNConv(str(Jinny.getNumOfDays())) + u'. \n' +
+                                                     u'It is my day ' + str(Jinny.getNumOfDays()) + u'. \n' +
                                                      u'Use /help for more options',
                                                 reply_markup=self.genKeyboard())
-                    elif chkNConv(msg['text']) == u'/help':
+                    elif msg['text'] == u'/help':
                         self.sender.sendMessage(text=u'/today - get today\'s date and my day count. \n' +
                                                      u'/help - for those who have bad memory. \n' +
                                                      u'/query - check my day count for a particular date. \n',
                                                 reply_markup=self.genKeyboard())
-                    elif chkNConv(msg['text']) == u'/query' or chkNConv(msg['text']) == u'某日係 day 幾':
+                    elif msg['text'] == u'/query' or msg['text'] == u'某日係 day 幾':
                         self._convert_type = ConverType.asking_date
                         self.sender.sendMessage(text=u'邊日呀 ?', reply_markup={'hide_keyboard': True})
-                    elif chkNConv(msg['text']) == u'/No' :
+                    elif msg['text'] == u'/No' :
                         self._convert_type = ConverType.nothing
                         self.sender.sendMessage(text=u'Bye !',
                                                 reply_markup=self.genKeyboard())
-                    elif chkNConv(msg['text']) == u'有野要買':
+                    elif msg['text'] == u'有野要買':
                         self._convert_type = ConverType.adding_to_buy
                         self.sender.sendMessage(text=u'買乜？', reply_markup={'hide_keyboard': True})
-                    elif chkNConv(msg['text']) == u'有乜未買？':
+                    elif msg['text'] == u'有乜未買？':
                         self.sender.sendMessage(text=self.genBuyList(), reply_markup=self.genKeyboard())
                     elif re.compile(u'買左\[\d+\].*').match(msg['text']) is None:
-                        match_obj = re.compile(u'買左\[(\d+)\].*').match(chkNConv(msg['text']))
+                        match_obj = re.compile(u'買左\[(\d+)\].*').match(msg['text'])
                         del_index = match_obj.group(1)
                         self.boughtItem(del_index=int(del_index))
 
@@ -138,19 +138,19 @@ class AngryJinny(telepot.helper.ChatHandler):
                                                      u'你試多次啦。或者用 /help 我就教你用。')
                 elif self._convert_type == ConverType.asking_date:
                     try :
-                        self.sender.sendMessage(text=u'For ' + chkNConv(msg['text']) + u', It is my day ' +
-                                                     chkNConv(str(Jinny.getNumOfDaysSpecific(msg['text']))) +
+                        self.sender.sendMessage(text=u'For ' + msg['text'] + u', It is my day ' +
+                                                     str(Jinny.getNumOfDaysSpecific(msg['text'])) +
                                                      u'. Any other date to ask ? /No ?')
                     except BaseException :
                         self.sender.sendMessage(text=u'Not a data that I can understand ! \n' +
                                                      u'What date (YYYYMMDD) ? \nOr you done ? ( /No )')
                 elif self._convert_type == ConverType.adding_to_buy:
-                    if chkNConv(msg['text']) == u'/done':
+                    if msg['text'] == u'/done':
                         self._convert_type = ConverType.nothing
                         self.doneWithBuyList()
                         self.writeListToFile()
                     else:
-                        to_buy_list.append(chkNConv(msg['text']))
+                        to_buy_list.append(msg['text'])
                         self.sender.sendMessage(text=u'仲有冇？如果冇，就用 /done 完結。', reply_markup={'hide_keyboard': True})
                 else:
                     self.sender.sendMessage(text=u'我唔明呀。\n' +
