c325979ebb2130ad19fc46f07b64914402c5f28e
Daedalus <daedalus.chan@gmail.com>
2018-11-23 08:03:46 +0800

    run pip install

--- chk_n_conv.py
+++
@@ -1,7 +0,0 @@
-def chkNConv(instr):
-    if isinstance(instr, str):
-        return instr.encode(encoding='utf-8')
-    elif isinstance(instr, unicode):
-        return instr
-    else:
-        return u''
